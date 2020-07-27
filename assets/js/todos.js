9b4aa800fcc05f00aa51c50f7767ace51e65f658
muskaan-ag09 <muskaan.agarwal28@gmail.com>
2020-07-27 20:04:22 +0530

    first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,43 @@
+// check of specific totos by clicking
+$("ul").on("click","li",function(){
+	//if li is grey then turn it black else turn it grey
+	// console.log($(this).css("color"));
+	// if($(this).css("color")==="rgb(128, 128, 128)"){
+	// 	$(this).css({
+	// 	color:
+	// 	"black",
+	// 	textDecoration: "none"
+	// });
+	// }
+	// else{
+	// 	$(this).css({
+	// 	color:
+	// 	"gray",
+	// 	textDecoration: "line-through"
+	// });
+	// }
+	$(this).toggleClass("completed");
+
+});
+
+//click on x to delete
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+	$(this).remove();	
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		//grabbing new todo text from input
+		var todoText=$(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> "+todoText+"</li>");
+	}
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
